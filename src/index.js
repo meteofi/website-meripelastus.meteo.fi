d08ae77e871ca04bac75cdf1160e8ce9e663be6a
Mikko Rauhala <mikko@rauhala.net>
2025-06-04 15:04:15 +0300

    fix: Separate vessel triangle and heading line into different SVG groups for better visibility

--- src/index.js
+++ src/index.js
@@ -577,8 +577,10 @@ function aisIcon(feature) {
   // Main vessel shape with transparent triangle and heading line (outline only)
   const svg =
     '<svg width="150" height="150" version="1.1" xmlns="http://www.w3.org/2000/svg">' +
-    '<g stroke="green" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" fill-opacity="1">' +
-    '<path d="M 100,75 L 50,90 L 50,60 z" style="fill:none;fill-opacity:0;fill-rule:evenodd;stroke-linejoin:round" />' +
+    '<g stroke="green" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" fill="none">' +
+    '<path d="M 100,75 L 50,90 L 50,60 z" />' +
+    '</g>' +
+    '<g stroke="green" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">' +
     headingline +
     '</g>' +
     '</svg>';
